327accdc372a9455f0f7006f01c82d155c3af7cb
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-07-13 23:50:50 +0900

    feat.User User CRUD 구현

---
+++ src/modules/user/dto/UserDto.ts
@@ -0,0 +1,100 @@
+import {IsString, IsEmail, IsOptional, MinLength, MaxLength, Matches, IsInt} from 'class-validator';
+import { Field } from "../../../utils/mapper/FieldNameExtractor";
+import {Role} from "../../../auth/authorization/Role";
+
+export class PostUserDto {
+    @IsString({ message: 'Name must be contained.' })
+    name: string;
+
+    @IsString({ message: 'Nickname must be contained.' })
+    nickname: string;
+
+    @IsEmail({}, { message: 'Invalid email address.' })
+    email: string;
+
+    @IsString({ message: 'Password must be contained.' })
+    @MinLength(8, { message: 'Password must be at least 8 characters long.' })
+    @MaxLength(20, { message: 'Password must be at most 20 characters long.' })
+    @Matches(/(?=.*[a-z])/, { message: 'Password must contain at least one lowercase letter.' })
+    @Matches(/(?=.*[0-9])/, { message: 'Password must contain at least one number.' })
+    @Matches(/(?=.*[!@#$%^&*])/, { message: 'Password must contain at least one special character.' })
+    password: string;
+
+    @IsString()
+    gender: string;
+
+    @IsString()
+    ageGroup: string;
+
+    @IsString()
+    mbti: string;
+
+    @IsString()
+    character: string;
+
+    @IsString()
+    voiceType: string;
+
+    @IsInt()
+    categoryId: number;
+
+}
+
+export class UpdateUserDto {
+
+    @IsOptional()
+    @IsInt()
+    ageGroup?: number;
+
+    @IsOptional()
+    @IsString()
+    mbti?: string;
+
+    @IsOptional()
+    @IsString()
+    nickname?: string;
+
+    @IsOptional()
+    @IsString()
+    character?: string;
+
+    @IsOptional()
+    @IsString()
+    voiceType?: string;
+
+}
+
+export class ResponseUserDto {
+    @Field
+    id: number;
+
+    @Field
+    name: string;
+
+    @Field
+    nickname: string;
+
+    @Field
+    email: string;
+
+    @Field
+    gender: string;
+
+    @Field
+    ageGroup: string;
+
+    @Field
+    mbti: string;
+
+    @Field
+    character: string;
+
+    @Field
+    voiceType: string;
+
+    @Field
+    categoryId: number;
+
+    @Field
+    role: Role;
+}
