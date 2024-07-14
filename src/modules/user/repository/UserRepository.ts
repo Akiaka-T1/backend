c58079ea409a2992bda47e5e0bff9063e3b8eb1a
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-07-14 09:37:49 +0900

    fix.UserController user List 요청시 페이지네이션 기능 수정

--- src/modules/user/repository/UserRepository.ts
+++ src/modules/user/repository/UserRepository.ts
@@ -1,6 +1,8 @@
 import { Repository, DataSource } from 'typeorm';
 import { User } from '../entity/User';
 import { Injectable } from '@nestjs/common';
+import {PaginationResult,PaginationOptions,paginate} from "../../../utils/pagination/pagination";
+
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -13,5 +15,8 @@ export class UserRepository extends Repository<User> {
 
     async findByEmail(email: string): Promise<User | undefined> {
         return this.findOne({ where: { email } });
+    }
+    async paginate(options: PaginationOptions): Promise<PaginationResult<User>> {
+        return paginate(this, options);
     }
 }
