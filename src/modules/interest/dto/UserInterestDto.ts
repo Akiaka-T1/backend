ccc13edf6dde665c2aff7298ae88bef3dcf55a96
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-07-17 22:31:23 +0900

    feat.relations 중간 엔티티 생성 알고리즘 변경

--- src/modules/interest/dto/UserInterestDto.ts
+++ src/modules/interest/dto/UserInterestDto.ts
@@ -1,13 +1,11 @@
 import { Field } from '../../../utils/mapper/FieldNameExtractor';
-import { ResponseInterestDto } from './InterestDto'
-import {Interest} from "../entity/Interest";
 
 export class ResponseUserInterestDto {
     @Field
     id: number;
 
     @Field
-    interest: Interest;
+    name: string;
 
     @Field
     score: number;
