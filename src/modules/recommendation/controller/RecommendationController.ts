f1f62f42acd3b00245c606f43ccc3ee7f5d04a4b
yoon-gang <sgi07151@naver.com>
2024-07-29 17:58:58 +0900

    feat. contorller

---
+++ src/modules/recommendation/controller/RecommendationController.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Patch, Body, Param, ParseIntPipe, Query, UseGuards } from '@nestjs/common';
+import { RecommendationService } from '../service/RecommendationService';
+import { AuthGuard } from '../../../auth/JwtAuthGuard/JwtAuthGuard';
+import { RolesGuard } from '../../../auth/authorization/RolesGuard';
+import { Roles } from '../../../auth/authorization/decorator';
+import { Role } from '../../../auth/authorization/Role';
+
+@Controller('api/recommendations')
+export class RecommendationController {
+  constructor(private readonly recommendationService: RecommendationService) {}
+
+  @Patch(':id/click')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.User)
+  async handlePostClick(@Body('token') token: string, @Param('id', ParseIntPipe) postId: number): Promise<void> {
+    return this.recommendationService.handlePostClick(token, postId);
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.User)
+  async getAllRecommendations(@Param('userId', ParseIntPipe) userId: number, @Query('ageGroup') ageGroup: string, @Query('mbti') mbti: string): Promise<any> {
+    const bestContent = await this.recommendationService.getBestContentByInterests(userId);
+    const popularContent = await this.recommendationService.getPopularContentByAgeGroup(ageGroup);
+    const recommendedContent = await this.recommendationService.getRecommendedContentByMBTI(mbti);
+
+    return {
+      bestContent,
+      popularContent,
+      recommendedContent
+    };
+  }
+}
