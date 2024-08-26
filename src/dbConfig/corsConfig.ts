e79bb616742ce3f7f7db6aa9224028a82851b3ba
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-08-27 00:14:55 +0900

    refactor.cors cors 주소 수정

--- src/dbConfig/corsConfig.ts
+++ src/dbConfig/corsConfig.ts
@@ -1,7 +1,7 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 export const corsOptions: CorsOptions = {
-  origin: ['https://meta2day.online','www.meta2day.online'],
+  origin: ['https://meta2day.online','https://www.meta2day.online'],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Authorization','Content-Type', 'Accept'],
   exposedHeaders: ['Authorization', 'Content-Type'],
